cbfc844da12a26611a550e9572074d247a9add2f
suhermano123 <suhermano123@gmail.com>
2025-02-27 21:21:31 -0500

    add

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 module.exports = {
   output: 'standalone',
-  experimental: {
-    srcDir: 'src',
-  },
+ 
   eslint: {
     ignoreDuringBuilds: true, // Ignorar los errores de ESLint durante la construcción
   },
