fb4f8c82b145838a16cb3da1a8683507a3cb7246
su hermano <suhermano123@gmail.com>
2026-03-09 18:27:20 -0500

    update content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return <Provider store={store}>
     <Head>
       <title>novapornx - Free Premium Adult HD Videos</title>
-      <meta name="description" content="Watch and download free premium adult videos in HD on novapornx." />
+      <meta name="description" content="novapornx is a top destination to watch free porn videos from some of the most popular premium adult websites online. Stream high-quality porn content from leading sites such as Brazzers, Blacked, Team Skeet, MYLF, Bangbros, Adult Time, and many others. All videos are available in the highest possible resolution, with frequent updates and fewer ads, making it the perfect place to enjoy free adult streaming." />
     </Head>
     <Component {...pageProps} />
   </Provider>
